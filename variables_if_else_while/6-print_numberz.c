ce716e348aaf8cdb47063035e6c30e2f6a347229
Charles-HBTN <charles.ekobo@gmail.com>
2024-10-25 13:49:46 +0000

    var_hbtn-6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
-{
+int void(main) {
 	int i;
 
-	for (i = 0; i < 10; i++)
-		printf("%d", i);
-	printf("\n");
+	for (i = 0; i <= 9; i++)
+		putchar(i + "0")
+	putchar('\n');
 
-	return (0);
+	return 0;
 }
