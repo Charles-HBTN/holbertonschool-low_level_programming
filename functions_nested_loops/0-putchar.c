75d1162079ef66937f2ac5409363b9e6273c12b1
Charles-HBTN <charles.ekobo@gmail.com>
2024-10-28 10:21:17 +0000

    ftnl_hbtn-0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Check description
+ * Return: 0.
+ */
+int main(void)
+{
+	char word[9] = "Holberton";
+	int i;
+
+	for (i = 0; i < 9; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+
+	return (0);
+}
