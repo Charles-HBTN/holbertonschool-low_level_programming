99eeddcc7de8a0194798df352499f4274bfae42f
Charles-HBTN <charles.ekobo@gmail.com>
2024-10-28 14:15:50 +0000

    ftnl_hbtn-0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,16 +2,15 @@
 #include "main.h"
 
 /**
- * main - Print _putchar
- * Return: Always 0
+ * print_alphabet - Check description
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Return: Nothing.
  */
-int main(void)                                                                                                                          
-{                                                                                                                                       
-    int ch;                                                                                                                         
-    for (ch = 'a'; ch <= 'z'; ch++)                                                                                                 
-    {                                                                                                                               
-        putchar (ch);                                                                                                                                                                                                                   
-    }    
-    putchar ('\n');     
-    return (0);
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
 }
