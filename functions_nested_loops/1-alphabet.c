dcee9b5fbd053ab1eff8482ee393f2377f173718
Charles-HBTN <charles.ekobo@gmail.com>
2024-10-29 10:09:25 +0000

    ftnl_hbtn-1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,13 +5,13 @@
  *
  * Return: Always 0.
  */
-void print_alphabet(void)
+int main(void)
 {
     int ch;                                                                                                                         
     for (ch = 'a'; ch <= 'z'; ch++)                                                                                                 
     {                                                                                                                               
-        putchar (ch);                                                                                                                                                                                                                   
+        _putchar (ch);                                                                                                                                                                                                                   
     }    
-    putchar ('\n');    
+    print_alphabet();
     return (0);
 }
