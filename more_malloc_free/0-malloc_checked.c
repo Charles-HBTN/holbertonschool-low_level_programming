436b995d61ce9816f55fad9f7cfb3946429c670d
Charles-HBTN <charles.ekobo@gmail.com>
2024-11-18 11:46:24 +0000

    mmal_hbtn-0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,17 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
-#define SIZE 1024
+#include "main.h"
 
-#endif
+/**
+ * malloc_checked - allocates memory using malloc, exit(98) if it fails
+ * @b: size of the memory block to be allocated
+ *
+ * Return: pointer to the address of the memory block
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *block;
+
+	block = malloc(b);
+	if (block == NULL)
+		exit(98);
+	return (block);
+}
